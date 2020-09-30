60d39f4e9b4c22a17bea8a9ca38b82be60a96b01
TamizhselvanR <40333860+TamizhselvanR@users.noreply.github.com>
2020-09-30 22:21:22 +0530

    replace for loop with in python

--- TCS_NQT_Day1_Slot1_Coding.py
+++ TCS_NQT_Day1_Slot1_Coding.py
@@ -1,15 +1,9 @@
 key=input()      # To get key from user
-keywords=['break','case','continue','default','defer','else'
+keywords=['break','case','continue','default','defer','else',
           'for','func','goto','if','map','range',
-          'return','struct','type','var']        # List with keywords
-flag=0                              # flag initilaization
-for word in keywords:
-    if(word==key):                   # if key found make flag as 1 and break the Loop
-        flag+=1
-        break
-if(flag==1):                          # If flag is 1 print found
+          'return','struct','type','var'] # List with keywords
+flag=0# flag initilaization
+if key in keywords:
     print("{} is a keyword".format(key))
-else:                                           # Else print not found
+else:
     print("{} is not a keyword".format(key))
-
-
